fdcf44222813ab11d944a7e5ae8c38abae834d56
Erfan Nateghi <erfannateghicpp@gmail.com>
2024-04-25 21:22:58 +0330

    image scanner page added

--- CCTV.py
+++ CCTV.py
@@ -1,5 +1,5 @@
 import cv2
-from PIL import ImageTk, Image
+from PIL import Image
 import face_recognition
 import os 
 import numpy as np
@@ -9,10 +9,10 @@ from ultralytics import YOLO
 import random
 import pickle
 
-
-
-
-
+YOLO_MODEL = None
+DETECTION_COLORS = []
+CLASS_LIST =[]
+FINAL_IMAGE = None
 
 class video:
 
@@ -30,46 +30,44 @@ class video:
             self.encodesCurFrame = []
             self.log_counter = 0
             self.last_log_time = time.time()-10
-            self.img_empty_camera = ImageTk.PhotoImage(file='emptyCamera3.png')
+            self.img_empty_camera = Image.open('emptyCamera3.png')
             self.cap = [None]*4
             self.results = None
             self.res = None
+            self.names_and_encoded_images = {}
             
-           
-            
-    
-
-
-
-
     #listing images and names
     def train(self):
+        global YOLO_MODEL
+        global DETECTION_COLORS
+        global CLASS_LIST
+
         # loading images if they are not already loaded in encoding.pkl
         try:
             with open('encodings.pkl', 'rb') as f:
-                names_and_encoded_images = pickle.load(f)
+                self.names_and_encoded_images = pickle.load(f)
         except FileNotFoundError:
-            names_and_encoded_images = {}
+            self.names_and_encoded_images = {}
         path = 'people'
         list = os.listdir(path)
         for cl in list:
             curImg = cv2.imread(f'{path}/{cl}')
             name = os.path.splitext(cl)[0]
             self.images.append(curImg)
-            if os.path.splitext(cl)[0] in names_and_encoded_images.keys(): # if in encoding.pkl skip the encoding
+            if os.path.splitext(cl)[0] in self.names_and_encoded_images.keys(): # if in encoding.pkl skip the encoding
                 self.namelist.append(name)
-                self.encodeList.append(names_and_encoded_images[os.path.splitext(cl)[0]][0])
-                names_and_encoded_images[os.path.splitext(cl)[0]][1] = True
+                self.encodeList.append(self.names_and_encoded_images[os.path.splitext(cl)[0]][0])
+                self.names_and_encoded_images[os.path.splitext(cl)[0]][1] = True
             else: # if not in encoding.pkl encode the image
                 self.namelist.append(name)
                 small_image = cv2.resize(curImg, (0, 0), fx=0.25, fy=0.25)
                 img = cv2.cvtColor(small_image, cv2.COLOR_BGR2RGB)
                 encode = face_recognition.face_encodings(img)[0]
                 self.encodeList.append(encode)
-                names_and_encoded_images[name] = [encode,True]
+                self.names_and_encoded_images[name] = [encode,True,time.time()]
 
         # make a copy of dic to remove false flags
-        updated_names_and_encoded_images = {name:[encode,flag] for (name, [encode, flag]) in names_and_encoded_images.items() if flag == True}
+        updated_names_and_encoded_images = {name:[encode,flag,last_seen] for (name, [encode, flag, last_seen]) in self.names_and_encoded_images.items() if flag == True}
         for name in updated_names_and_encoded_images.keys():
             updated_names_and_encoded_images[name][1] = False # initialize flag to false
         # Save encodings and names to a file for later use
@@ -78,27 +76,24 @@ class video:
 
 
         # Load YOLOv8 model
-        self.my_file = open("object_list/objects.txt", "r")
-        self.data = self.my_file.read()
-        self.class_list = self.data.split("\n")
-        self.my_file.close()
-        self.model = YOLO('yolov8n.pt')
-        self.detection_colors = []
-        for i in range(len(self.class_list)):
+        my_file = open("object_list/objects.txt", "r")
+        data = my_file.read()
+        CLASS_LIST = data.split("\n")
+        my_file.close()
+        YOLO_MODEL = YOLO('yolov8n.pt')
+        DETECTION_COLORS = []
+        for i in range(len(CLASS_LIST)):
             r = random.randint(0, 255)
             g = random.randint(0, 255)
             b = random.randint(0, 255)
-            self.detection_colors.append((b, g, r))
+            DETECTION_COLORS.append((b, g, r))
         
-
-
-   
     def detect_objects(self, frame):
         
         # for better performance it can be change to 2 so that it detects objects in every 2nd frame
         if self.frame_counter2 % 1 == 0:
             # Get detected objects and their bounding boxes
-            self.results = self.model.predict(source=[frame], conf=0.45, save=False)
+            self.results = YOLO_MODEL.predict(source=[frame], conf=0.45, save=False)
             self.res = self.results[0].numpy()
             
                 
@@ -112,19 +107,19 @@ class video:
 
                     boxes = self.results[0].boxes
                     box = boxes[i]  # returns one box
-                    clsID = box.cls.numpy()[0]
+                    objectID = box.cls.numpy()[0]
                     conf = box.conf.numpy()[0]
                     bb = box.xyxy.numpy()[0]
 
                     cv2.rectangle(frame,
                         (int(bb[0]), int(bb[1])),
                         (int(bb[2]), int(bb[3])),
-                        self.detection_colors[int(clsID)],3,)
+                        DETECTION_COLORS[int(objectID)],3,)
 
             # Display class name and confidence
                     font = cv2.FONT_HERSHEY_COMPLEX
                     cv2.putText(frame,
-                        self.class_list[int(clsID)] + " " + str(round(conf, 3)) + "%",
+                        CLASS_LIST[int(objectID)] + " " + str(round(conf, 3)) + "%",
                         (int(bb[0]), int(bb[1]) - 10),
                         font,1,(255, 255, 255),2,
                     )
@@ -145,7 +140,7 @@ class video:
             faceDis = face_recognition.face_distance(self.encodeList,encodeFace)
             matchIndex = np.argmin(faceDis)
             if matches[matchIndex]:
-                name = self.namelist[matchIndex].upper()
+                name = self.namelist[matchIndex]
             else:
                 name = 'UNKNOWN'
             #print(name)
@@ -160,10 +155,19 @@ class video:
 
             # save a line of log containing the name of the person and the time but every 10 seconds for each person
             current_time = time.time()
-            
-            if current_time - self.last_log_time > 10:
-                    self.last_log_time = current_time
-                # open the file in append mode
+            # for every name there is a last_seen value which shows the last date the person was seen
+            # we use this to check if the person has been seen in the last 10 seconds so that we can save a line of log
+            if name == 'UNKNOWN':
+                if current_time - self.last_log_time > 10:
+                        self.last_log_time = current_time
+                    # open the file in append mode
+                        with open('log.txt', 'a') as f:
+                            f.write(f'{name} at {time.strftime("%Y-%m-%d %H:%M:%S")}\n')
+                            f.close()
+            else:
+                if self.names_and_encoded_images[name][2] < current_time - 10:
+                    self.names_and_encoded_images[name][2] = current_time
+                    # open the file in append mode
                     with open('log.txt', 'a') as f:
                         f.write(f'{name} at {time.strftime("%Y-%m-%d %H:%M:%S")}\n')
                         f.close()
@@ -174,48 +178,158 @@ class video:
         
         return frame
         
+    def update(self):
+        for i in range(4):
+            if self.cap[i].isOpened():
+                ret, frame = self.cap[i].read()
+                if ret:
+                    
+                    # make frames smaller so it takes less time to process
+                    small_frame = cv2.resize(frame, (0, 0), fx=0.25, fy=0.25)
+                    
+                    # Detect faces and draw frames around them
+                    if self.face_detection:
+                        frame_with_faces_object = self.detect_faces(frame,small_frame)
+                    else:
+                        frame_with_faces_object = frame
 
-    
-    def update(self,i):
-        ret, frame = self.cap[i].read()
-        if ret:
-            
-            # make frames smaller so it takes less time to process
-            small_frame = cv2.resize(frame, (0, 0), fx=0.25, fy=0.25)
-            
-            # Detect faces and draw frames around them
-            if self.face_detection:
-                frame_with_faces_object = self.detect_faces(frame,small_frame)
+                    if self.object_detection:
+                        frame_with_faces_object = self.detect_objects(frame_with_faces_object)
+                    else:
+                        pass
+                    
+
+                    # Convert OpenCV image to PIL format
+                    rgb_img = cv2.cvtColor(frame_with_faces_object, cv2.COLOR_BGR2RGB)
+                    
+                    pil_img = Image.fromarray(rgb_img)
+                    
+                    img_tk = CTkImage(pil_img, size=(self.root.winfo_width()/4.5, self.root.winfo_height()/3.5))
+                    # Update label with new image
+                    self.video_label[i].img = img_tk
+                    self.video_label[i].configure(image=img_tk)
             else:
-                frame_with_faces_object = frame
+                self.video_label[i].configure(image= CTkImage(Image.open('emptyCamera3.png'),size=(self.root.winfo_width()/4.5, self.root.winfo_height()/3.5)))
+            
+        self.root.after(1,self.update)
 
-            if self.object_detection:
-                frame_with_faces_object = self.detect_objects(frame_with_faces_object)
-            else:
-                pass
-            
 
-            # Convert OpenCV image to PIL format
-            rgb_img = cv2.cvtColor(frame_with_faces_object, cv2.COLOR_BGR2RGB)
-            
-            pil_img = Image.fromarray(rgb_img)
-            resized_img = pil_img.resize((480, 360), Image.LANCZOS)
-            img_tk = ImageTk.PhotoImage(resized_img)
-            # Update label with new image
-            self.video_label[i].img = img_tk
-            self.video_label[i].configure(image=img_tk)
-            
-        
-        
-        self.root.after(1,self.update,i)
 
     def video_stream(self):
         for i in range(4):
             self.cap[i] = cv2.VideoCapture(i)
-            if not self.cap[i].isOpened():
-                self.video_label[i].configure(image=self.img_empty_camera)
-            self.update(i)
+        self.update()
 
+            
+
+class image_scanner:
+    def __init__(self,root,scanner_label,face_detection,object_detection,image_size,info_listbox_people,info_listbox_objects):
+        self.root = root
+        self.scanner_label = scanner_label
+        self.default_image = None
+        self.face_detection = face_detection
+        self.object_detection = object_detection
+        try:
+            with open('encodings.pkl', 'rb') as f:
+                self.names_and_encoded_images = pickle.load(f)
+        except FileNotFoundError:
+            self.names_and_encoded_images = {}
+        self.image_size = image_size
+        self.info_listbox_people = info_listbox_people
+        self.info_listbox_objects = info_listbox_objects
+
+
+
+    # scan for faces
+    def detect_faces(self,default_image_RGB, small_image):
+        faceloc = face_recognition.face_locations(small_image)
+        encode = face_recognition.face_encodings(small_image,faceloc)
+        for face_encoding, face_location,i in zip(encode, faceloc,range(len(faceloc))):
+            list_of_encodings = [value[0] for key, value in self.names_and_encoded_images.items()]
+            list_of_names = [key for key, value in self.names_and_encoded_images.items()]
+            matches = face_recognition.compare_faces(list_of_encodings,face_encoding)
+            faceDis = face_recognition.face_distance(list_of_encodings,face_encoding)
+            matchIndex = np.argmin(faceDis)
+            if matches[matchIndex]:
+                name = list_of_names[matchIndex]
+            else:
+                name = 'UNKNOWN'
+
+            self.info_listbox_people.insert(i,name)# insert into listbox
+            # draw rectangle and name on image
+            top, right, bottom, left = face_location
+            top , right, bottom, left = top*4, right*4, bottom*4, left*4
+            cv2.rectangle(default_image_RGB, (left, top), (right, bottom), (255, 0, 0), 2)
+            cv2.putText(default_image_RGB, name, (left, top - 10), cv2.FONT_HERSHEY_SIMPLEX, 3, (36, 255, 12), 2)
+        return default_image_RGB
+
+    def detect_objects(self,image_with_faces):
+        results = YOLO_MODEL.predict(source=[image_with_faces], conf=0.45, save=False)
+        isResult = results[0].numpy() 
+
+        if len(isResult) != 0:
+                for i in range(len(results[0])):
+
+                    boxes = results[0].boxes
+                    box = boxes[i]  # returns one box
+                    objectID = box.cls.numpy()[0]
+                    conf = box.conf.numpy()[0]
+                    bb = box.xyxy.numpy()[0]
+                    
+                    cv2.rectangle(image_with_faces,# draw rectangle
+                        (int(bb[0]), int(bb[1])),
+                        (int(bb[2]), int(bb[3])),
+                        DETECTION_COLORS[int(objectID)],3,)
+                    
+                   
+
+                    # Display class name and confidence
+                    font = cv2.FONT_HERSHEY_COMPLEX
+                    cv2.putText(image_with_faces,
+                        CLASS_LIST[int(objectID)] + " " + str(round(conf, 3)) + "%",
+                        (int(bb[0]), int(bb[1]) - 10),
+                        font,3,(255, 255, 255),2,
+                    )
+
+                    self.info_listbox_objects.insert(i,CLASS_LIST[int(objectID)]) # insert name of object in listbox
+                    
+
+
+        return image_with_faces
     
 
-                
+    def save_image(self,path):
+        if FINAL_IMAGE != None:
+            FINAL_IMAGE.save(path)
+    
+
+
+    def scan(self,default_image_path):
+        global FINAL_IMAGE
+        # clear listboxs
+        self.info_listbox_people.delete(0,'end')
+        self.info_listbox_objects.delete(0,'end')
+        # load and convert image to rgb
+        default_image = cv2.imread(default_image_path)
+        default_image_RGB = cv2.cvtColor(default_image, cv2.COLOR_BGR2RGB)
+        # resize image for better speed
+        small_image = cv2.resize(default_image_RGB, (0, 0), fx=0.25, fy=0.25)
+
+        # scan image
+        if self.face_detection:
+            image_with_faces_object = self.detect_faces(default_image_RGB,small_image)
+        else:
+            image_with_faces_object = default_image_RGB
+        if self.object_detection:
+            image_with_faces_object = self.detect_objects(image_with_faces_object)
+        
+        # convert numpy array to PIL image
+        pil_image = Image.fromarray(image_with_faces_object)
+        # convert PIL image to ctk image
+        img_ctk = CTkImage(pil_image, size=(self.image_size[0],self.image_size[1]))
+        self.scanner_label.configure(image=img_ctk)
+
+        FINAL_IMAGE = pil_image
+
+        
+
--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-from PIL import ImageTk
 from customtkinter import *
 from CCTV import *
+from tkinter import filedialog , Label ,PhotoImage, Canvas
+from PIL import Image , ImageTk
+import CTkListbox
+
+
+default_image_path = None
 
 def update_log():
     try:
@@ -21,30 +26,55 @@ def update_face_detection(value):
 def update_object_detection(value):
     CCTV.object_detection = value
 
+def update_face_detection_image(value):
+    image_sc.face_detection = value
+
+def update_object_detection_image(value):
+    image_sc.object_detection = value
+
+def insert_image(no_image_size):
+    global default_image_path
+    default_image_path = filedialog.askopenfilename(initialdir="/", title="Select an image",
+                                           filetypes=( [("Image Files", "*.jpg; *.png")]))
+    if default_image_path != None:
+        image_label_insert.configure(image=CTkImage(Image.open(default_image_path),size=(no_image_size[0],no_image_size[1])))
+
+def image_save():
+    final_image_save_path = filedialog.asksaveasfile(mode='w', defaultextension=".png",filetypes=(("JPEG file", "*.jpg"),("PNG file", "*.png"),("All Files", "*.*")),title="Where do you want to save your image?")
+
+    if final_image_save_path != None:
+        image_sc.save_image(final_image_save_path)
 
 
 root = CTk()
-root.geometry('1800x1100')
+root.geometry('1920x1080')
+root.minsize(800, 600)
 root.title("CCTV AI")
 
-img_empty_camera = ImageTk.PhotoImage(file='emptyCamera3.png')
+img_empty_camera = CTkImage(Image.open('emptyCamera3.png'),size=(root.winfo_width()*2,root.winfo_width()*1.5))
+no_image = CTkImage(Image.open('noImage.jpg'),size=(root.winfo_width()*3,root.winfo_width()*2.25))
+no_image_size = (root.winfo_width()*3,root.winfo_width()*2.25)
 
 
 # create a notebook for tabs
-tabControl = CTkTabview(root, width=1700,height=1000,corner_radius=20)
+tabControl = CTkTabview(root, width=1700,height=1000,corner_radius=50)
 tabControl.pack(pady=20)
 
 tab1 = tabControl.add('    CCTV    ')
-tab2 = tabControl.add('   IMAGEs   ')
-tab3 = tabControl.add(' ADD PERSON ')
+tab2 = tabControl.add(' Image Scanner ')
+tab3 = tabControl.add(' Add Person ')
 tabControl._segmented_button.configure( height=50,font=('Arial',30))
 
+
+
+# tab 1
+#------------------------------------------------------------------------------------------------------------------------
 # log and options frame
 option_and_log_frame = CTkFrame(tab1,fg_color='#1B1B1B',width=400, height=500)
 option_and_log_frame.pack(fill="both", side='right')
 
 log_textbox = CTkTextbox(option_and_log_frame, width=300, height=400)
-log_textbox.pack(anchor='s', padx=30, pady=30)
+log_textbox.pack(anchor='s', padx=30, pady=30 )
 
 update_log_button = CTkButton(option_and_log_frame, text='Update Log', command=update_log,font=('',30),width=200,height=50,corner_radius=20)
 update_log_button.pack(pady=20)
@@ -61,7 +91,7 @@ checkbox_object_detection.pack(pady=40)
 
 # frame for video
 video_frame = CTkFrame(tab1)
-video_frame.pack(fill="both", side='left', padx=50, pady=50,)
+video_frame.pack(fill="both", side='left', padx=50, pady=50)
 
 video_label = []
 # label for displaying video
@@ -74,10 +104,75 @@ video_label[2].grid(row=1 , column= 0, padx=50, pady=50)
 video_label.append(CTkLabel(video_frame, text=''))
 video_label[3].grid(row=1 , column= 1, padx=50, pady=50)
 
-# Create video instance and start video streaming in a separate thread
+#------------------------------------------------------------------------------------------------------------------------
+
+#tab2
+#------------------------------------------------------------------------------------------------------------------------
+# split the tab into two (insert image and scan image)
+insert_image_frame = CTkFrame(tab2 , bg_color='#2B2B2B', fg_color='#1B1B1B',corner_radius=50)
+insert_image_frame.pack(fill="both",expand=True, side='left',padx=30, pady=30)
+
+scan_image_frame = CTkFrame(tab2, bg_color='#2B2B2B', fg_color='#1B1B1B',corner_radius=50)
+scan_image_frame.pack(fill="both",expand=True , side='right',padx=30, pady=30)
+# a lable which will display the addressed image
+image_label_insert = CTkLabel(insert_image_frame, text='',image=no_image)
+image_label_insert.pack(fill="both",pady=50, side='top')
+image_label_scan = CTkLabel(scan_image_frame, text='',image=no_image)
+image_label_scan.pack(fill="both", pady=50, side='top')
+
+
+# split the frame into two (insert button and scan options)
+insert_button_frame = CTkFrame(insert_image_frame,fg_color='#1B1B1B')
+insert_button_frame.pack(padx=30,anchor="s", fill="both",expand=True , side='left')
+
+scan_options_frame = CTkFrame(insert_image_frame,fg_color='#1B1B1B')
+scan_options_frame.pack( padx=30,anchor="s",fill='both',expand=True, side='right')
+
+# button for insert image
+insert_button = CTkButton(insert_button_frame, text='Insert Image', font=('', 30), width=200, height=50, corner_radius=20,command=lambda: insert_image(no_image_size))
+insert_button.pack(pady=40)
+# 2 switch for scan mode (face and object detection)
+face_detection_switch = CTkSwitch(scan_options_frame, text='Face Detection', font=('', 30), variable=BooleanVar(value=True),command=lambda: update_face_detection_image(face_detection_switch.get()))
+face_detection_switch.pack(pady=40 , anchor='w')
+object_detection_switch = CTkSwitch(scan_options_frame, text='Object Detection', font=('', 30), variable=BooleanVar(value=True),command=lambda: update_object_detection_image(object_detection_switch.get()))
+object_detection_switch.pack(pady=(20,40),anchor='w')
+
+# split the frame into two (scan button and scan image , image information listbox)
+save_scan_frame = CTkFrame(scan_image_frame,fg_color='#1B1B1B')
+save_scan_frame.pack(padx=30,anchor="s", fill="both",expand=True , side='left')
+
+image_info_frame = CTkFrame(scan_image_frame,fg_color='#1B1B1B')
+image_info_frame.pack( padx=30,anchor="s",fill='both',expand=True, side='right')
+
+# button for scan image
+scan_button = CTkButton(save_scan_frame, text='Scan Image', font=('', 30), width=200, height=50, corner_radius=20,command=lambda: image_sc.scan(default_image_path))
+scan_button.pack(pady=20, anchor='center')
+save_image_button = CTkButton(save_scan_frame, text='Save Image', font=('', 30), width=200, height=50, corner_radius=20,command=image_save)
+save_image_button.pack(pady=20, anchor='center')
+
+# list box for image information
+people_frame = CTkFrame(image_info_frame,fg_color='#1B1B1B')
+people_frame.pack(pady=20,padx=(0,10),anchor="s", fill="both",expand=True , side='left')
+people_label = CTkLabel(people_frame, text='People Found', font=('', 20))
+people_label.pack()
+info_listbox_people = CTkListbox.CTkListbox(people_frame, width=100, height=400, font=('', 20), fg_color='#1B1B1B', bg_color='#1B1B1B')
+info_listbox_people.pack(fill= 'both', expand=True)
+
+objects_frame = CTkFrame(image_info_frame,fg_color='#1B1B1B')
+objects_frame.pack(pady=20,anchor="s", fill="both",expand=True , side='right')
+people_label = CTkLabel(objects_frame, text='Objects Found', font=('', 20))
+people_label.pack()
+info_listbox_objects = CTkListbox.CTkListbox(objects_frame, width=100, height=400, font=('', 20), fg_color='#1B1B1B', bg_color='#1B1B1B')
+info_listbox_objects.pack( fill= 'both', expand=True)
+
+#------------------------------------------------------------------------------------------------------------------------
+# Create video instance and start video streaming
 CCTV = video(root,video_label,checkbox_face_detection._variable.get(),checkbox_object_detection._variable.get())
 CCTV.train()
 CCTV.video_stream()
+
+image_sc = image_scanner(root,image_label_scan,face_detection_switch._variable.get(),object_detection_switch._variable.get(),no_image_size,info_listbox_people,info_listbox_objects)
+
 
 
 
